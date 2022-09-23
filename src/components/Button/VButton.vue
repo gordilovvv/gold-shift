0543dc41a91b288509f31b6a55baa5876fc9fef7
Igor <igorgordilov22@gmail.com>
2022-09-23 22:03:13 +0300

    VButton & VContainer

---
+++ src/components/Button/VButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+type VButtonProps = {
+    view: 'inverse'
+}
+
+withDefaults(defineProps<VButtonProps>(), {
+    view: 'inverse'
+});
+</script>
+
+<template>
+    <button :class="[styles.button, styles[view]]">
+        <span :class="styles.buttonContent">
+            <slot />
+        </span>
+    </button>
+</template>
+
+<style lang="scss" module="styles">
+@import './VButton.module.scss';
+</style>
---
+++ src/components/Container/VContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <div :class="styles.container">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss" module="styles">
+@import './VContainer.module.scss';
+</style>
