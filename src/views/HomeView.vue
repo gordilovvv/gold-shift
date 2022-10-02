897cadea2e21862cea0a50234783c2e5040c27ad
Igor <igorgordilov22@gmail.com>
2022-10-03 00:46:48 +0300

    some files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import HeroSection from '../components/Sections/Hero/HeroSection.vue';
+import AdvantagesSection from '../components/Sections/Advantages/AdvantagesSection.vue';
+import CurrenciesSection from '../components/Sections/Currencies/CurrenciesSection.vue';
+import GuideSection from '../components/Sections/Guide/GuideSection.vue';
+import AffiliateSection from '../components/Sections/Affiliate/AffiliateSection.vue';
+</script>
+
 <template>
-    sss
+    <HeroSection />
+    <AdvantagesSection />
+    <CurrenciesSection />
+    <GuideSection />
+    <AffiliateSection />
 </template>
