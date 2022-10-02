5d6b44eb5392f3f9267128b458773b4ff6702847
Igor <igorgordilov22@gmail.com>
2022-10-03 00:46:35 +0300

    configs

---
+++ src/config/guide.ts
@@ -0,0 +1,23 @@
+type Guide = {
+    title: string,
+    image: string
+}
+
+export const guide: Guide[] = [
+    {
+        title: 'Активируйте бота',
+        image: '/images/guide-1.png'
+    },
+    {
+        title: 'Выберите монету и тип сделки',
+        image: '/images/guide-2.png'
+    },
+    {
+        title: 'Укажите метод платежа и ознакомьтесь с условиями продавца',
+        image: '/images/guide-3.png'
+    },
+    {
+        title: 'Отправьте запрос и следуйте инструкциям до окончания сделки',
+        image: '/images/guide-4.png'
+    }
+];
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,21 @@
+export const PROJECT_NAME = 'Goldshift';
+
 export const TELEGRAM = {
     username: '@goldshift_bot',
     link: 'https://t.me/goldshift_bot'
 };
+
+export const AFFILIATE_LINK = TELEGRAM.link;
+
+export const SOCIALS: {icon: string, link: string}[] = [
+    {
+        icon: 'twitter',
+        link: 'https://twitter.com'
+    },
+    {
+        icon: 'facebook',
+        link: 'https://facebook.com'
+    }
+];
 
 export const ICONS = import.meta.glob('../assets/icons/*.svg', { eager: true });
